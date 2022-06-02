e7e62c8855ed10981811fcd873fb21acfc7d4362
ad115am <twente333@gmail.com>
2022-06-02 18:35:02 +0200

    Dodano metodę, która zwraca wyniki tylko dla tramwajów (bez autobusow)

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from google.transit import gtfs_realtime_pb2
 import requests
+
+def get_only_trams(vehicle_id):
+  if 1<vehicle_id<999:
+    return True
+  else:
+    return False
+
 def download_feeds_pb():
   feed = gtfs_realtime_pb2.FeedMessage()
   response = requests.get('https://www.ztm.poznan.pl/pl/dla-deweloperow/getGtfsRtFile/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ0ZXN0Mi56dG0ucG96bmFuLnBsIiwiY29kZSI6MSwibG9naW4iOiJtaFRvcm8iLCJ0aW1lc3RhbXAiOjE1MTM5NDQ4MTJ9.ND6_VN06FZxRfgVylJghAoKp4zZv6_yZVBu_1-yahlo&file=feeds.pb')
@@ -18,7 +25,8 @@ def download_vehicle_positions_pb():
   # print(a)
   for entity in feed.entity:
     #if entity.HasField('trip_update'):
-    print(f'id: {entity.vehicle.vehicle.id} route_id: {entity.vehicle.trip.route_id} lat: {entity.vehicle.position.latitude} lon: {entity.vehicle.position.longitude}')
+    if(get_only_trams(int(entity.vehicle.vehicle.id))):
+      print(f'id: {entity.vehicle.vehicle.id} route_id: {entity.vehicle.trip.route_id} lat: {entity.vehicle.position.latitude} lon: {entity.vehicle.position.longitude} speed : {entity.vehicle.position.speed}')
 
 
 
