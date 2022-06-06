85b37e7926f5d2ba17915dea2eb1a19ac07008fe
ad115am <twente333@gmail.com>
2022-06-06 23:21:16 +0200

    -poprawiono metode zwracajaca dany typ tramwaju. Teraz dziala show_trams_on_map dziala zarowno dla wszystkich jak i dla konkretnych

--- map.py
+++ map.py
@@ -8,15 +8,23 @@ my_map = folium.Map(
     zoom_start=12
 )
 tram_data=main.create_data_frame_from_tram_data(main.get_tram_data())
-def show_trams_on_map(*filter):
+def show_trams_on_map(filter):
     filter=filter
     valid_id= range(0,1000)
-    if filter=='double-sided':
-        valid_id= range(900,999)
+    print(filter)
+    if filter== 'double-sided':
+        valid_id = range(900,999)
+    elif filter == 'siemens':
+        valid_id = range(501, 515)
+    elif filter == 'all':
+        pass
+
+
 
 
     for _,tram in tram_data.iterrows():
         if int(tram['id']) in valid_id:
+            print(valid_id)
             tram_id = int(tram['id'])
             route_id = int(tram['route_id'])
             delay = int(tram['delay'])
@@ -44,5 +52,6 @@ def show_trams_on_map(*filter):
 # a=[]
 # b=main.create_data_frame_from_tram_data(main.get_tram_data())
 # print(b.at[0,'lat'])
-show_trams_on_map()
+show_trams_on_map('all')
 #show_trams_on_map('double-sided')
+#show_trams_on_map('siemens')
