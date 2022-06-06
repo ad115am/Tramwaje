1f8d7101613083db24b0ca4bd5d39216821c26d2
ad115am <twente333@gmail.com>
2022-06-06 23:24:19 +0200

    Usunieto niepotrzebne printy i poprawiono czytelnosc kodu

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import requests
 import pandas as pd
 
 trams_dataframe = pd.DataFrame()
+
+
 def get_only_trams(vehicle_id):
     if 1 < vehicle_id < 999:
         return True
@@ -20,7 +22,6 @@ def download_feeds_pb():
         print(entity)
 
 
-
 def download_vehicle_positions_pb_old():
     feed = gtfs_realtime_pb2.FeedMessage()
     response = requests.get(
@@ -30,11 +31,10 @@ def download_vehicle_positions_pb_old():
     feed.ParseFromString(response.content)
 
     for entity in feed.entity:
-        # if entity.HasField('trip_update'):
         if get_only_trams(int(entity.vehicle.vehicle.id)):
             print(
                 f'id: {entity.vehicle.vehicle.id} route_id: {entity.vehicle.trip.route_id} lat: {entity.vehicle.position.latitude}'
-                f' lon: {entity.vehicle.position.longitude} speed: {round(entity.vehicle.position.speed*3.6)} km/h')
+                f' lon: {entity.vehicle.position.longitude} speed: {round(entity.vehicle.position.speed * 3.6)} km/h')
 
 
 def download_trip_updates_pb_old():
@@ -49,7 +49,9 @@ def download_trip_updates_pb_old():
         if get_only_trams(int(entity.trip_update.vehicle.id)):
             print(
                 f'id: {entity.trip_update.vehicle.id} route_id: {entity.trip_update.trip.route_id} '
-                f'stop sequence: {entity.trip_update.stop_time_update[0].stop_sequence} delay: {entity.trip_update.stop_time_update[0].arrival.delay}')
+                f'stop sequence: {entity.trip_update.stop_time_update[0].stop_sequence} '
+                f'delay: {entity.trip_update.stop_time_update[0].arrival.delay}')
+
 
 def download_vehicle_positions_pb():
     feed = gtfs_realtime_pb2.FeedMessage()
@@ -59,6 +61,7 @@ def download_vehicle_positions_pb():
         'ND6_VN06FZxRfgVylJghAoKp4zZv6_yZVBu_1-yahlo&file=vehicle_positions.pb')
     return feed, response
 
+
 def download_trip_updates_pb():
     feed = gtfs_realtime_pb2.FeedMessage()
     response = requests.get(
@@ -67,71 +70,47 @@ def download_trip_updates_pb():
         'ND6_VN06FZxRfgVylJghAoKp4zZv6_yZVBu_1-yahlo&file=trip_updates.pb')
     return feed, response
 
+
 # to download both at the same time so that the data match
 def get_tram_data():
     vehicle_positions_pb_feed, vehicle_positions_pb_response = download_vehicle_positions_pb()
     trip_updates_pb_feed, trip_updates_pb_response = download_trip_updates_pb()
     vehicle_positions_pb_feed.ParseFromString(vehicle_positions_pb_response.content)
     trip_updates_pb_feed.ParseFromString(trip_updates_pb_response.content)
-    # for entity in vehicle_positions_pb_feed.entity:
-    #     # if entity.HasField('trip_update'):
-    #     if get_only_trams(int(entity.vehicle.vehicle.id)):
-    #         print(
-    #             f'id: {entity.vehicle.vehicle.id} route_id: {entity.vehicle.trip.route_id} lat: {entity.vehicle.position.latitude}'
-    #             f' lon: {entity.vehicle.position.longitude} speed: {round(entity.vehicle.position.speed * 3.6)} km/h')
-    # for entity in trip_updates_pb_feed.entity:
-    #     if get_only_trams(int(entity.trip_update.vehicle.id)):
-    #         print(
-    #             f'id: {entity.trip_update.vehicle.id} route_id: {entity.trip_update.trip.route_id} '
-    #             f'stop sequence: {entity.trip_update.stop_time_update[0].stop_sequence} delay: {entity.trip_update.stop_time_update[0].arrival.delay}')
     return vehicle_positions_pb_feed.entity, trip_updates_pb_feed.entity
 
 
 def create_data_frame_from_tram_data(tram_data):
-    vehicle_positions_list=[]
-    trip_updates_list=[]
+    vehicle_positions_list = []
+    trip_updates_list = []
     for entity in tram_data[0]:
         if get_only_trams(int(entity.vehicle.vehicle.id)):
-            vehicle_positions_list.append([int(entity.vehicle.vehicle.id),int(entity.vehicle.trip.route_id),entity.vehicle.position.latitude,
-                          entity.vehicle.position.longitude,round(entity.vehicle.position.speed * 3.6)])
+            vehicle_positions_list.append(
+                [int(entity.vehicle.vehicle.id), int(entity.vehicle.trip.route_id), entity.vehicle.position.latitude,
+                 entity.vehicle.position.longitude, round(entity.vehicle.position.speed * 3.6)])
 
-
-
-        # if get_only_trams(int(entity.vehicle.vehicle.id)):
-        #     print(
-        #         f'id: {entity.vehicle.vehicle.id} route_id: {entity.vehicle.trip.route_id} lat: {entity.vehicle.position.latitude}'
-        #         f' lon: {entity.vehicle.position.longitude} speed: {round(entity.vehicle.position.speed * 3.6)} km/h')
     for entity in tram_data[1]:
         if get_only_trams(int(entity.trip_update.vehicle.id)):
             trip_updates_list.append(
-            [int(entity.trip_update.vehicle.id), int(entity.trip_update.trip.route_id),
-             entity.trip_update.stop_time_update[0].stop_sequence,
-             entity.trip_update.stop_time_update[0].arrival.delay
-             ])
-        #print(entity)
-        #dataframe1.append(tram_data[1])
-        # if get_only_trams(int(entity.trip_update.vehicle.id)):
-        #     print(
-        #         f'id: {entity.trip_update.vehicle.id} route_id: {entity.trip_update.trip.route_id} '
-        #         f'stop sequence: {entity.trip_update.stop_time_update[0].stop_sequence} delay: {entity.trip_update.stop_time_update[0].arrival.delay}')
+                [int(entity.trip_update.vehicle.id), int(entity.trip_update.trip.route_id),
+                 entity.trip_update.stop_time_update[0].stop_sequence,
+                 entity.trip_update.stop_time_update[0].arrival.delay
+                 ])
     vehicle_positions_columns = ["id", "route_id", "lat", "lon", "speed"]
     trip_updates_columns = ["id", "route_id", "stop_seq", "delay"]
-    data_frame_vehicle_positions = pd.DataFrame(vehicle_positions_list,columns=vehicle_positions_columns)
-    data_frame_trip_updates = pd.DataFrame(trip_updates_list,columns=trip_updates_columns)
+    data_frame_vehicle_positions = pd.DataFrame(vehicle_positions_list, columns=vehicle_positions_columns)
+    data_frame_trip_updates = pd.DataFrame(trip_updates_list, columns=trip_updates_columns)
     data_frame_merged = data_frame_vehicle_positions.merge(data_frame_trip_updates, how='inner')
     # print(data_frame_vehicle_positions)
     # print(data_frame_trip_updates)
-    #print(data_frame_merged.to_string())
+    # print(data_frame_merged.to_string())
     # print(vehicle_positions_list)
     # print(trip_updates_list)
-    return(data_frame_merged)
+    return (data_frame_merged)
 
 
-
-
-
-#download_feeds_pb()
-#download_trip_updates_pb()
-#download_vehicle_positions_pb()
-#get_tram_data()
+# download_feeds_pb()
+# download_trip_updates_pb()
+# download_vehicle_positions_pb()
+# get_tram_data()
 create_data_frame_from_tram_data(get_tram_data())
--- map.py
+++ map.py
@@ -4,54 +4,47 @@ import webbrowser
 import main
 
 my_map = folium.Map(
-    location=[52.388980865478516,16.89756965637207],
+    location=[52.388980865478516, 16.89756965637207],
     zoom_start=12
 )
-tram_data=main.create_data_frame_from_tram_data(main.get_tram_data())
+tram_data = main.create_data_frame_from_tram_data(main.get_tram_data())
+
+
 def show_trams_on_map(filter):
-    filter=filter
-    valid_id= range(0,1000)
+    filter = filter
+    valid_id = range(0, 1000)
     print(filter)
-    if filter== 'double-sided':
-        valid_id = range(900,999)
+    if filter == 'double-sided':
+        valid_id = range(900, 999)
     elif filter == 'siemens':
         valid_id = range(501, 515)
     elif filter == 'all':
         pass
 
-
-
-
-    for _,tram in tram_data.iterrows():
+    for _, tram in tram_data.iterrows():
         if int(tram['id']) in valid_id:
-            print(valid_id)
             tram_id = int(tram['id'])
             route_id = int(tram['route_id'])
             delay = int(tram['delay'])
             speed = int(tram['speed'])
-            icon_size=(50,50)
             if delay > 120:
-                icon = folium.Icon(color='red',icon="subway",prefix='fa')
-            elif delay<-120:
+                icon = folium.Icon(color='red', icon="subway", prefix='fa')
+            elif delay < -120:
                 icon = folium.Icon(color='green', icon="subway", prefix='fa')
             else:
-                icon = folium.Icon(color='blue',icon="subway",prefix='fa')
+                icon = folium.Icon(color='blue', icon="subway", prefix='fa')
             folium.Marker(
-                location=[tram['lat'],tram['lon']],
+                location=[tram['lat'], tram['lon']],
                 popup=int(tram['id']),
                 tooltip=f'id: {tram_id}, route: {route_id}, delay: {delay}, speed: {speed}',
                 icon=icon
-
 
             ).add_to(my_map)
     print(my_map)
     my_map.save("map.html")
     webbrowser.open("map.html")
-#marker1 = folium.Marker(location=[52.388980865478516,16.89756965637207]).add_to(my_map)
 
-# a=[]
-# b=main.create_data_frame_from_tram_data(main.get_tram_data())
-# print(b.at[0,'lat'])
+
 show_trams_on_map('all')
-#show_trams_on_map('double-sided')
-#show_trams_on_map('siemens')
+# show_trams_on_map('double-sided')
+# show_trams_on_map('siemens')
